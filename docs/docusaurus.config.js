9233acf1c15bd595952410c78cd0e1196b61b587
DDlia <gh@ddlia.com>
2024-01-28 05:31:32 +0000

    init

---
+++ docs/docusaurus.config.js
@@ -0,0 +1,126 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'TechnoTUT NOC',
+  tagline: 'TechnoTUT Network Operation Center Team',
+  favicon: 'img/logo.png',
+
+  // Set the production url of your site here
+  url: 'https://noc.technotut.net',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'TechnoTUT', // Usually your GitHub org/user name.
+  projectName: 'Infrastructure', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'ja',
+    locales: ['ja'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: '/',
+          sidebarPath: './sidebars.js',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/TechnoTUT/Infrastructure/tree/dev/',
+        },
+        blog: false,
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  markdown: {
+    mermaid: true,
+  },
+  themes: ['@docusaurus/theme-mermaid'],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/technotut_noc.png',
+      navbar: {
+        title: 'Network Operation Center Team',
+        logo: {
+          alt: 'TechnoTUT Logo',
+          src: 'img/logo.png',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: 'Index',
+          },
+          {
+            href: 'https://github.com/TechnoTUT/Infrastructure/',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Home',
+                href: 'https://technotut.net',
+              },
+              {
+                label: 'Docs',
+                href: 'https://docs.technotut.net',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/toyohashitechno',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/TechnoTUT',
+              },
+              {
+                label: 'Instagram',
+                href: 'https://instagram.com/tut_technotut',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} TechnoTUT. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
